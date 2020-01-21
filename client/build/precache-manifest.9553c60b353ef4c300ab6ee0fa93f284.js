8ba469d4a6fbaa03631f671e8a43ae6077903f5c
Marshall Sykes <rmsykes91@gmail.com>
2020-01-21 13:16:05 -0500

    first commit

---
+++ client/build/precache-manifest.9553c60b353ef4c300ab6ee0fa93f284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423106cfbde2fd44362639b949f02994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44319b30557c0dbf353",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "f44319b30557c0dbf353",
+    "url": "/static/js/main.8b374f3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
