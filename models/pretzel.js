d08c65a6cd63f135f658ce5ae8aafead06adf0c3
Marshall Sykes <rmsykes91@gmail.com>
2020-01-21 13:31:36 -0500

    began models/pretzel.js, building crud funcitons now

--- models/pretzel.js
+++ models/pretzel.js
@@ -1,57 +1,33 @@
-/* 
- * Place all functions, classes, and/or DB schemas here for a single 
- * model.
- */
+// IMPORT MONGOOSE
+const mongoose = require('./connection.js')
 
-/* Step 1
- *
- * TODO: import mongoose connection
- * NOTE: skip this if you are not using mongoose
- *
- */
-//const mongoose = require('./connection.js')
 
-/* Step 1 alternative
- *
- * TODO: make a global variable to act as an in memory database. 
- * NOTE: doing this WILL NOT persist your data and you will loose
- * your data once you stop running your server.
- *
- */
-global.sampleModel = [];
 
-/* Step 2
- *
- * TODO: create model schema 
- * NOTE: skip this if you are not using mongoose
- *
- */
-//const SampleModelSchema = new mongoose.Schema({
-//  name: String
-//})
+// CREATE SCEMA
+const PretzelSchema = new mongoose.Schema({
+ name: String
+})
 
-/* Step 3
- *
- * TODO: create collection API
- * NOTE: skip this if you are not using mongoose
- *
- */
-//const SampleCollection = mongoose.model('Sample', SampleModelSchema)
+// CREATE PRETZEL API
+const PretzelCollection = mongoose.model('Pretzel', PretzelSchema)
 
-/* Step 4
- *
- * TODO: delete this it's just a sample
- *
- */
-function getHelloWorldString() {
-  return 'hello world'
-}
 
-/* Step 5
- *
- * TODO: export all functions from this file by adding their names as keys to this
- * object
- */
+
+// CRUD FUNCTIONS
+
+// getAllPretzels()
+
+// getOnePretzel()
+
+// createPretzel()
+
+// updatePretzel()
+
+// deletePretzel()
+
+
+
+// EXPORT
 module.exports = {
   getHelloWorldString
 }
