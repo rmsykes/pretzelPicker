7913af7b003250324f10eb396460e0432bf4758b
Marshall Sykes <rmsykes91@gmail.com>
2020-01-22 14:28:13 -0500

    began working on HomePage Component

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,32 @@
+// Import React, { Component }, Axios, and { Link }
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+
+
+// CREATE AND EXPORT HOMEPAGE COMPONENT
+export default class HomePage extends Component {
+
+    // HomePage Component State
+    state = {
+        message: ''
+    }
+
+    // componentDidMount() - used to bring in data from backend, don't need data right now, but have it ready to pull in /api/pretzelPicker data but need to change state template
+    componentDidMount() {
+        axios.get('/api/pretzelPicker')
+            .then((res) => {
+                this.setState({ message: res.data })
+            })
+    }
+
+    // Rendered in Browser
+    render() {
+        return (
+            <div>
+                <h1>Pretzel Picker</h1>
+            </div>
+        )
+    }
+}
