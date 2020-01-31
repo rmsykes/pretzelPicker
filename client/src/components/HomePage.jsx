50329e31eaa66f41ce1094718375fc9245d57d13
Marshall Sykes <rmsykes91@gmail.com>
2020-01-31 17:47:59 -0500

    added react boostrap nav bar do home page. going to add to other pages now

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Button, Form, FormControl, FormGroup, Input, FormCheck } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
+
 
 
 // Create and export HomePage Component
@@ -10,24 +12,39 @@ export default class HomePage extends Component {
 
     // HomePage Component State
     state = {
-        
+
     }
 
     // componentDidMount() - used to bring in data from backend, don't need data right now, but have it ready to pull in /api/pretzelPicker data but need to change state template
     componentDidMount() {
         axios.get('/')
             .then((res) => {
-                this.setState({  })
+                this.setState({})
             })
     }
 
-    
+
     // Rendered in Browser
     render() {
 
-        
+
         return (
             <div className='homePage'>
+
+                {/* Bootstratp nav bar */}
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="/">Pretzel Picker</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/users">Users</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-info">Search</Button>
+                    </Form>
+                </Navbar>
+
 
                 <div className='pageHeader'>
                     <nav>
@@ -43,10 +60,10 @@ export default class HomePage extends Component {
                     <h2>Do you like pretzels? <br /> Want to keep track of the best pretzels? <br /> You're in the right place! </h2>
                 </div>
 
-            
 
 
-                        
+
+
 
             </div>
         )
