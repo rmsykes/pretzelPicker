ec1c05f7792a01e48d10f0480caba162aace031b
Marshall Sykes <rmsykes91@gmail.com>
2020-02-05 13:22:51 -0500

    reformatted some of the singlePretzel page. still needs more css but its a start

--- client/src/components/SinglePretzel.jsx
+++ client/src/components/SinglePretzel.jsx
@@ -48,55 +48,63 @@ export default class SinglePretzel extends Component {
 
         // variable and if statement are for rendering if theres cheese, show yes, if not show no
         let isThereCheese = ''
-        if (this.state.pretzel.cheese === true){
+        if (this.state.pretzel.cheese === true) {
             isThereCheese = ' yes'
         } else isThereCheese = ' no'
 
         // variable and if statement are for rendering if theres mustard, show yes, if not show no
         let isThereMustard = ''
-        if (this.state.pretzel.mustard === true){
+        if (this.state.pretzel.mustard === true) {
             isThereMustard = ' yes'
         } else isThereMustard = ' no'
 
 
         return (
 
-            
+
 
             this.state.isRedirect ? <Redirect to="/user" /> :
 
 
-            <div className='singlePretzelPage'>
+                <div className='singlePretzelPage'>
 
-                <div>
-                    {/* Bootstratp nav bar */}
-                    < Navbar bg="dark" variant="dark" >
-                        <Navbar.Brand href="/">Pretzel Picker</Navbar.Brand>
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/user">Users</Nav.Link>
-                        </Nav>
-                    </Navbar>
+                    <div>
+                        {/* Bootstratp nav bar */}
+                        < Navbar bg="dark" variant="dark" >
+                            <Navbar.Brand href="/">Pretzel Picker</Navbar.Brand>
+                            <Nav className="mr-auto">
+                                <Nav.Link href="/user">Users</Nav.Link>
+                            </Nav>
+                        </Navbar>
+                    </div>
+
+
+                    <h1>Pretzel Picker</h1>
+
+                    <div className='singlePretzelPagePretzelBody'>
+
+                        <div className='singlePretzelPageInfo'>
+                            <h2>{this.state.pretzel.restaurantName} </h2>
+                            <h2>Rank: {this.state.pretzel.ranking}/5</h2>
+                            <h2>${this.state.pretzel.price}</h2>
+                            <h2>Cheese:{isThereCheese}</h2>
+                            <h2>Mustard:{isThereMustard}</h2>
+                        </div>
+
+                        <div className='singlePretzelPagePretzelPhoto'>
+                            <img className='singlePretzelPagePhoto' src={this.state.pretzel.photo} alt="pretzel photo" />
+                        </div>
+                    </div>
+
+                    <div className='singlePretzelPagePretzelNotes'>
+                        <h2>Notes: {this.state.pretzel.notes}</h2>
+                    </div>
+
+
+                    <button id='deletePretzelButton' onClick={() => this.deletePretzel()}>Delete This Pretzel</button>
+
+
                 </div>
-
-
-                <h1>Pretzel Picker</h1>
-
-                <div className='singlePretzelPagePretzelInfo'>
-
-                    <h2>{this.state.pretzel.restaurantName} </h2>
-                    <h2>Rank: {this.state.pretzel.ranking}/5</h2>
-                    <h2>${this.state.pretzel.price}</h2>
-                    <h2>Cheese:{isThereCheese}</h2>
-                    <h2>Mustard:{isThereMustard}</h2>
-                    <h2>Photo: <img className='singlePretzelPagePhoto' src={this.state.pretzel.photo} alt="pretzel photo" /></h2>
-                    <h2>Notes: {this.state.pretzel.notes}</h2>
-
-                </div>
-
-                <button id='deletePretzelButton' onClick={() => this.deletePretzel()}>Delete This Pretzel</button>
-
-
-            </div>
         )
     }
 }
