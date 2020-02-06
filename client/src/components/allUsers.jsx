32d6286b05604c2d43bf467373b472979f78a44b
Marshall Sykes <rmsykes91@gmail.com>
2020-02-05 19:13:41 -0500

    added search field for searching users on all users page

--- client/src/components/allUsers.jsx
+++ client/src/components/allUsers.jsx
@@ -13,6 +13,8 @@ export default class AllUsers extends Component {
     // all user state
     state = {
         listOfUsers: [],
+        query: '',
+        filteredData: []
     }
 
 
@@ -22,21 +24,49 @@ export default class AllUsers extends Component {
             .then((res) => {
                 this.setState({ listOfUsers: res.data })
             })
+        this.getData();
+
+
     }
+
+
+    // sets query state targeting the search input form 
+    handleInputChange = event => {
+        const query = event.target.value;
+
+        this.setState(prevState => {
+            const filteredData = prevState.data.filter(element => {
+                return element.name.toLowerCase().includes(query.toLowerCase());
+            });
+
+            return {
+                query,
+                filteredData
+            };
+        });
+    };
+
+
+
+
+    getData = () => {
+        fetch(`/api/user`)
+            .then(response => response.json())
+            .then(data => {
+                const { query } = this.state;
+                const filteredData = data.filter(element => {
+                    return element.name.toLowerCase().includes(query.toLowerCase());
+                });
+
+                this.setState({
+                    data,
+                    filteredData
+                });
+            });
+    };
 
     // render in browser
     render() {
-
-
-        const listOfUsers = this.state.listOfUsers.map(
-            (user) => {
-                return <div>
-                    <Link to={`/user/${user._id}`}>
-                        <h5 className='allUsersUserListNameAndPhoto'><img src={user.photo} alt="user's photo"/> <br/> {user.name} </h5>
-                    </Link>
-                </div>
-            }
-        )
 
 
         return (
@@ -47,15 +77,30 @@ export default class AllUsers extends Component {
                     <Navbar.Brand href="/">Pretzel Picker</Navbar.Brand>
                     <Nav className="mr-auto">
                         <Nav.Link href="/user">Users</Nav.Link>
-                        
+
                     </Nav>
-                    
+
                 </Navbar>
 
 
                 <h1>Pretzel People</h1>
 
-                {listOfUsers}
+                {/* search form field for filtering users */}
+                <div className="searchForm">
+                    <form>
+                        <input
+                            placeholder="Search for..."
+                            value={this.state.query}
+                            onChange={this.handleInputChange}
+                        />
+                    </form>
+                    <div className='usersListFilteredSearch'>{this.state.filteredData.map(user =>
+
+                        <Link to={`/user/${user._id}`}>
+                            <p>{user.name} <img src={user.photo} alt="user photo" /></p>
+                        </Link>
+                    )}</div>
+                </div>
             </div>
         )
     }
